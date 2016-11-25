1d559eca45fa6b7a4ca00cede7726ea3c69b14b3
MatAff <mathijsaffourtit@gmail.com>
2016-11-24 20:40:38 -0700

    Tank updates

--- MA/Tank.py
+++ MA/Tank.py
@@ -3,7 +3,7 @@
 import sys
 import time
 import atexit
-sys.path.append("/home/pi/Adafruit-Motor-HAT-Python-Library") ### ADD PATH
+sys.path.append("/home/pi/Documents/Robots/slcypi/HAT_Python3") ### ADD PATH
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 
 class Tank():
@@ -12,11 +12,15 @@ class Tank():
     def __init__(self) -> None:
         """Initialize method"""
         mh = Adafruit_MotorHAT(addr=0x60)
-        leftMotor = mh.getMotor(1)
-        rightMotor = mh.getMotor(2)
-        atexit.register(turnOffMotors)
-    
-    def turnOffMotors(self) -> None:
+        self.leftMotor = mh.getMotor(1)
+        self.rightMotor = mh.getMotor(2)
+        #atexit.register(turnOffMotors)
+        mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+    def turnOffMotors(self):
         """Method to turn off all motors"""
         mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
         mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
@@ -25,47 +29,47 @@ class Tank():
 
     def stop(self) -> None:
         """Method to stop driving"""
-        turnOffMotors()
+        self.turnOffMotors()
         
     def drive(self,direction, speed=100) -> None:
         """Method control forward speed
         direction <integer> {-1,0,1}
         speed <integer> {0:255}"""
         if direction == 1:
-                leftMotor.run(Adafruit_MotorHAT.FORWARD)
-                rightMotor.run(Adafruit_MotorHAT.FORWARD)
-                leftMotor.setSpeed(speed)
-                rightMotor.setSpeed(speed)
+                self.leftMotor.run(Adafruit_MotorHAT.FORWARD)
+                self.rightMotor.run(Adafruit_MotorHAT.FORWARD)
+                self.leftMotor.setSpeed(speed)
+                self.rightMotor.setSpeed(speed)
         if direction == -1:
-                leftMotor.run(Adafruit_MotorHAT.BACKWARD)
-                rightMotor.run(Adafruit_MotorHAT.BACKWARD)
-                leftMotor.setSpeed(speed)
-                rightMotor.setSpeed(speed)
+                self.leftMotor.run(Adafruit_MotorHAT.BACKWARD)
+                self.rightMotor.run(Adafruit_MotorHAT.BACKWARD)
+                self.leftMotor.setSpeed(speed)
+                self.rightMotor.setSpeed(speed)
         if direction == 0:
-                leftMotor.setSpeed(0)
-                rightMotor.setSpeed(0)
-                leftMotor.run(Adafruit_MotorHAT.RELEASE)
-                rightMotor.run(Adafruit_MotorHAT.RELEASE)
+                self.leftMotor.setSpeed(0)
+                self.rightMotor.setSpeed(0)
+                self.leftMotor.run(Adafruit_MotorHAT.RELEASE)
+                self.rightMotor.run(Adafruit_MotorHAT.RELEASE)
 
     def rotate(self,direction, speed=30):
         """Method to control steering
         direction <integer> {-1,0,1}
         speed <integer>"""
         if direction == 1:
-                leftMotor.run(Adafruit_MotorHAT.FORWARD)
-                rightMotor.run(Adafruit_MotorHAT.BACKWARD)
-                leftMotor.setSpeed(speed)
-                rightMotor.setSpeed(speed)
+                self.leftMotor.run(Adafruit_MotorHAT.FORWARD)
+                self.rightMotor.run(Adafruit_MotorHAT.BACKWARD)
+                self.leftMotor.setSpeed(speed)
+                self.rightMotor.setSpeed(speed)
         if direction == -1:
-                leftMotor.run(Adafruit_MotorHAT.BACKWARD)
-                rightMotor.run(Adafruit_MotorHAT.FORWARD)
-                leftMotor.setSpeed(speed)
-                rightMotor.setSpeed(speed)
+                self.leftMotor.run(Adafruit_MotorHAT.BACKWARD)
+                self.rightMotor.run(Adafruit_MotorHAT.FORWARD)
+                self.leftMotor.setSpeed(speed)
+                self.rightMotor.setSpeed(speed)
         if direction == 0:
-                leftMotor.setSpeed(0)
-                rightMotor.setSpeed(0)
-                leftMotor.run(Adafruit_MotorHAT.RELEASE)
-                rightMotor.run(Adafruit_MotorHAT.RELEASE)
+                self.leftMotor.setSpeed(0)
+                self.rightMotor.setSpeed(0)
+                self.leftMotor.run(Adafruit_MotorHAT.RELEASE)
+                self.rightMotor.run(Adafruit_MotorHAT.RELEASE)
 
 
 
--- TestTank.py
+++ TestTank.py
@@ -2,13 +2,14 @@ import sys
 from time import sleep
 
 sys.path.append("/home/pi/Documents/Robots/slcypi/MA") ### ADD PATH
+sys.path.append("/home/pi/Documents/Robots/slcypi/HAT_Python3") ### ADD PATH
 
 
 #from MA.Tank import Tank
-import Tank
+from Tank import Tank
 
-myRobot = tank.Tank()
+myRobot = Tank()
 
 myRobot.drive(1,50)
-time.sleep(1)
+sleep(1)
 myRobot.stop()
