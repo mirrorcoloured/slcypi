7ef0608a7b6834357a14a9b12e1a0d655e544eff
MatAff <mathijsaffourtit@gmail.com>
2016-12-07 14:34:18 -0700

    Added stereo example

--- Stereo/TestStereo.py
+++ Stereo/TestStereo.py
@@ -2,9 +2,18 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
  
-imgL = cv2.imread('tsukuba_l.png',0)
-imgR = cv2.imread('tsukuba_r.png',0)
- 
+imgL = cv2.imread('imL.png',0)
+imgR = cv2.imread('imR.png',0)
+
+print imgL.shape, imgR.shape
+
+# Print class
+print(type(imgL).__name__)
+
+# Show image DOESNT WORK
+plt.imshow(imgL,'gray')
+plt.show()
+
 stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
 disparity = stereo.compute(imgL,imgR)
 plt.imshow(disparity,'gray')
