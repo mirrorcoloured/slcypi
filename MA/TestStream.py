d1ec738acfaac397c210bcc9d2d3418e2f559d2f
MatAff <mathijsaffourtit@gmail.com>
2016-12-12 20:38:27 -0700

    Merge

--- MA/TestStream.py
+++ MA/TestStream.py
@@ -7,9 +7,15 @@ with picamera.PiCamera() as camera:
         camera.resolution = (320, 240)
 
         while True:
+            print("HI")
             camera.capture(stream, 'bgr', use_video_port=True)
             # stream.array now contains the image data in BGR order
+            print("HI")
+            #cv2.startWindowThread()
+            #cv2.namedWindow("Frame")
             cv2.imshow('frame', stream.array)
+            print("HI")
+            
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             # reset the stream before the next capture
