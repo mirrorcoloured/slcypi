04618bc782d9f50b83db0c77665bea1cb5ad9dbf
MatAff <mathijsaffourtit@gmail.com>
2016-12-19 20:59:49 -0700

    Update/redo ImageAnalysis class

--- MA/ImageAnalysis.py
+++ MA/ImageAnalysis.py
@@ -3,190 +3,76 @@
 import pygame
 import pygame.camera
 from PIL import Image
-#from pylab import *
-
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 class ImageAnalysis():
-    """Class with methods for image analysis"""
+        """Class with methods for image analysis"""
 
-    def __init__(self):
-        """Initialize method"""
-        print("Initiate ImageAnalysis")
+        def __init__(self):
+                """Initialize method"""
+                print("Initiate ImageAnalysis")
 
-        # Set target pixel
-        #self.tp = (100,90,30)
-        #self.tp = (180,130,70)
-        self.tp = (130,110,20)
-        self.distBenchmark = 1500
+                # Set starting values
+                WITDH = 320
+                HEIGHT = 240
+                filterLower = np.array([25,35,70])
+                filterUpper = np.array([75,255,205])
+                blockAnalyseYstart = 0
+                blockAnalyseYend = 100
+
+        def edgeDetection(self, bgr):
+                laplacian = cv2.Laplacian(bgr,cv2.CV_64F)
+                return(laplacian)
+                #sobelx = cv2.Sobel(frame,cv2.CV_64F,1,0,ksize=5)
+                #sobely = cv2.Sobel(frame,cv2.CV_64F,0,1,ksize=5)
+
+        # Method to apply color filter
+        def colorFilter(self, bgr, erode = False, dilate = False):
+                hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
+                mask = cv2.inRange(hsv, self.filterLower, self.filterUpper)
+                if erode == True:
+                        kernel = np.ones((5,5),np.uint8)
+                        mask = cv2.erode(mask,kernel,iterations = 1)
+                if dilate == True:
+                        kernel = np.ones((5,5),np.uint8)
+                        mask = cv2.dilate(mask,kernel,iterations = 1)
+                res = cv2.bitwise_and(frame, frame, mask=mask)
+                return(res, mask)
         
-    # Method to convert pixel int to RGB values
-    def getRGB(self,pixelInt):
-        #print(pixelInt)
-        blue = pixelInt & 255
-        green = (pixelInt >> 8) & 255
-        red = (pixelInt >> 16) & 255
-        myArray = [red, green, blue]
-        return myArray
+        def smooth(self,img):
+                kernel = np.ones((15,15),np.float32)/225
+                smoothed = cv2.filter2D(img,-1,kernel)
+                return(smoothed)
 
-    def showMidPixel(self,img,w,h):
+        def blurring(self,img):
+                blur = cv2.GaussianBlur(img,(15,15),0)
+                return(blur)
 
-        # Transform        
-        pxarray = pygame.PixelArray(img)
-        middlePixel = pxarray[int(w/2),int(h/2)]
-        
-        #middlePixel = pxarray[150,150]
-        print(middlePixel)
-        rgb = self.getRGB(middlePixel)
-        print(rgb)
+        def medianBlurring(self,img):
+                median = cv2.medianBlur(img,15)
+                return(median)
 
-    def setTarget(self,img,w,h):
-        """Method to find RGB values target.
-        Target area middle of bottom quarter of camera view."""
-        print("Setting target")
-        pxarray = pygame.PixelArray(img)
-        count = 0
-        sumR = 0
-        sumG = 0
-        sumB = 0
-        for x in range(int(w*0.4),int(w*0.6)):
-            for y in range(int(h*0.75),int(h-1)):
-                px = pxarray[x,y]
-                rgb = self.getRGB(px)
-                count = count + 1
-                sumR = sumR + rgb[0]
-                sumG = sumG + rgb[1]
-                sumB = sumB + rgb[2]
-        av = [int(sumR/count),int(sumG/count),int(sumB/count)]
-        print(av)
-        self.tp = av
-        return av
-        
+        def bilateralBlur(self,img):
+                bilateral = cv2.bilateralFilter(img,15,75,75)
+                return(bilateral)
 
-    def checkPixel(self,px):
-        rgb = self.getRGB(px)       
-        distance = (self.tp[0] - rgb[0]) ** 2 + (self.tp[1] - rgb[1]) ** 2 +(self.tp[2] - rgb[2]) ** 2
-        #print(distance)
-        if distance < self.distBenchmark:
-                return True
-        else:
-                return False
+        def blockAnalyze(self,mask):
+                # Assumes 320 width
+                sum = 0
+                count = 0
+                for x in range(5):
+                        blockCount = np.sum(mask[x*64:x*64+63,self.blockAnalyseYstart:blockAnalyseYend]) / 255     
+                        sum = sum + blockCount * x
+                        count = count + blockCount
 
-    def convertTrueFalse(self,img,w,h):
-        print("Convert")
-        pxarray = pygame.PixelArray(img)
-        for x in range(0,int((w-1)),2):
-            print(x)
-            for y in range(0,int((h-1)),2):
-                check = self.checkPixel(pxarray[x,y])
-                #print(check)
-                if check ==True:
-                    pxarray[x,y] = 50000
+                if count > 0:
+                        overallMean = float(sum) / count        
+                        direction = (overallMean - 2) / 2
+                        return direction, count
                 else:
-                    pxarray[x,y] = 0
-
-        img2 = pygame.PixelArray.make_surface(pxarray)
-        return img2
-
-    def convertRainbow(self,img,w,h):
-        print("Convert")
-        pxarray = pygame.PixelArray(img)
-        for x in range(0,int((w-1)),2):
-            print(x)
-            for y in range(0,int((h-1)),2):
-                rgb = self.getRGB(pxarray[x,y])       
-                distance = (self.tp[0] - rgb[0]) ** 2 + (self.tp[1] - rgb[1]) ** 2 +(self.tp[2] - rgb[2]) ** 2
-                if distance <= 1000:
-                    pxarray[x,y] = 80000
-                else:
-                    if distance <= 2000:
-                        pxarray[x,y] = 70000
-                    else:
-                        if distance <= 3000:
-                            pxarray[x,y] = 60000
-                        else:
-                            if distance <= 4000:
-                                pxarray[x,y] = 50000
-                            else:
-                                if distance <= 5000:
-                                    pxarray[x,y] = 40000
-                                else:
-                                    if distance <= 6000:
-                                        pxarray[x,y] = 30000
-                                    else:
-                                        if distance <= 7000:
-                                            pxarray[x,y] = 20000
-                                        else:
-                                            if distance <= 8000:
-                                                pxarray[x,y] = 10000
-                                            
-        img2 = pygame.PixelArray.make_surface(pxarray)
-        return img2
+                        return -999, count
 
 
-    def getLinePosition(self,img,w,h):
-        pxarray = pygame.PixelArray(img)
-        startY = 0.3
-        endY = 0.6
-        sum = 0
-        count = 0.1
-        for x in range(0,w):
-            for y in range(int(h*startY),int(h*endY)):
-                check = self.checkPixel(pxarray[x,y])
-                if check ==True:
-                    sum = sum + x
-                    count = count + 1
-        
-        if count > 5:
-            # Compute average
-            average = sum / count
-        
-            # standardize
-            standardVal = (average - (w / 2)) / (w /2) 
-        
-            return standardVal
-        else:
-            return -999
-        
-
-# Method to analyze image to follow line
-def analyzeLine(img):
-
-        # Settings
-        directionCutpoint = 0.5
-
-        # Transform
-        pxarray = pygame.PixelArray(img)
-        #print pxarray.shape
-        
-        # Loop through
-        sum = 0
-        count = 0.1
-        for y in range(110,130):
-                for x in range(0,321):
-
-                        # Check
-                        check = self.checkPixel(pxarray[x,y])
-                        sum = sum + x
-                        count = count + 1
-
-        # Compute average
-        average = sum / count
-        print(average)
-        
-        # standardize
-        standardVal = (average - (640 / 2)) / (640 /2) 
-        print(standardVal)
-
-        # Compare to diction cut point
-        direction = 0
-        if standardVal < 0- directionCutpoint:
-                direction = 1
-        if standardVal > directionCutpoint:
-                direction = -1
-
-        # Return direction
-        return direction
-
-
-
-
+    
---
+++ MA/Robot_V002k.py
@@ -0,0 +1,183 @@
+# Import statements
+import sys
+sys.path.append("/home/pi/Documents/Robots/slcypi/MA") ### ADD PATH
+sys.path.append("/home/pi/Documents/Robots/slcypi/HAT_Python3") ### ADD PATH
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from Tank import Tank
+from ImageAnalysis import ImageAnalysis
+import picamera
+import picamera.array
+import time
+import pygame
+from scipy import ndimage
+from time import sleep
+
+# Settings
+WIDTH = 320
+HEIGHT = 240
+
+# Initialize Tank
+robot = Tank()
+robot.correctDirections(True,True,True)
+
+# Initialize ImageAnalysis
+IA = ImageAnalysis()
+IA.filterLower = np.array([25,35,70])
+IA.filterUpper = np.array([75,255,205])
+
+# Initialize Pygame
+pygame.init()
+pygame.display.set_caption('My Robot')
+screen = pygame.display.set_mode((WIDTH,HEIGHT),0)
+
+# Start settings
+auto = False 
+done = False
+startTime = time.time()
+with picamera.PiCamera() as camera:
+        with picamera.array.PiRGBArray(camera) as stream:
+                camera.resolution = (WIDTH, HEIGHT)
+
+                while done == False:
+
+                        # Image capture
+                        camera.capture(stream, 'bgr', use_video_port=True)
+                        bgr = stream.array                        
+                        
+                        # Image process
+                        res, maks = IA.colorFilter(bgr, False, False)
+
+                        # Image transpose
+                        res = cv2.transpose(res)
+                        mask = np.transpose(mask)
+
+                        # Image display
+                        sface = pygame.surfarray.make_surface(res)                        
+                        screen.blit(sface,(0,0))
+                        pygame.display.update()            
+                        
+                        # User events
+                        for event in pygame.event.get():
+                                if event.type == pygame.KEYDOWN:
+                                        if (event.key == pygame.K_ESCAPE):
+                                                done = True
+                                        if event.key == (pygame.K_UP):
+                                                robot.driveSync(1)
+                                        if event.key == (pygame.K_DOWN):
+                                                robot.driveSync(-1)
+                                        if (event.key == pygame.K_LEFT):
+                                                robot.rotateSync(1,45)
+                                        if (event.key == pygame.K_RIGHT):
+                                                robot.rotateSync(-1,45)                                        
+                                        if (event.key == pygame.K_q):
+                                                auto = True
+                                        if (event.key == pygame.K_w):
+                                                auto = False
+                                                robot.driveSync(0)
+                                                robot.rotateSync(0)
+                                        if (event.key == pygame.K_7):
+                                                upper[0] = upper[0] + 5
+                                                print(upper)
+                                        if (event.key == pygame.K_u):
+                                                upper[0] = upper[0] - 5
+                                                print(upper)
+                                        if (event.key == pygame.K_j):
+                                                lower[0] = lower[0] + 5
+                                                print(lower)
+                                        if (event.key == pygame.K_m):
+                                                lower[0] = lower[0] - 5
+                                                print(lower)
+
+                                        if (event.key == pygame.K_8):
+                                                upper[1] = upper[1] + 5
+                                                print(upper)
+                                        if (event.key == pygame.K_i):
+                                                upper[1] = upper[1] - 5
+                                                print(upper)
+                                        if (event.key == pygame.K_k):
+                                                lower[1] = lower[1] + 5
+                                                print(lower)
+                                        if (event.key == pygame.K_COMMA):
+                                                lower[1] = lower[1] - 5
+                                                print(lower)
+
+                                        if (event.key == pygame.K_9):
+                                                upper[2] = upper[2] + 5
+                                                print(upper)
+                                        if (event.key == pygame.K_o):
+                                                upper[2] = upper[2] - 5
+                                                print(upper)
+                                        if (event.key == pygame.K_l):
+                                                lower[2] = lower[2] + 5
+                                                print(lower)
+                                        if (event.key == pygame.K_PERIOD):
+                                                lower[2] = lower[2] - 5
+                                                print(lower)
+                                                
+                                if event.type == pygame.KEYUP:
+                                        if event.key == (pygame.K_UP):
+                                                robot.driveSync(0)
+                                        if event.key == (pygame.K_DOWN):
+                                                robot.driveSync(0)
+                                        if (event.key == pygame.K_LEFT):
+                                                robot.rotateSync(0)
+                                        if (event.key == pygame.K_RIGHT):
+                                                robot.rotateSync(0)
+
+                        
+                        # Autonomous
+                        if auto == True:
+                                
+                                # Analyze line
+                                aRes = IA.blockAnalyze(mask)
+                                print(aRes)                        
+                                dir = aRes[0]
+                                count = aRes[1]
+                
+                                # Drive         
+                                if abs(dir) > 0.20:
+                                        rotateSpeed = 50
+                                        if abs(dir) > 0.5:
+                                                rotateSpeed = 70
+                                        if dir > 0:
+                                                print("Rotate -1")
+                                                #robot.driveSync(0)
+                                                robot.rotateSync(-1, rotateSpeed)
+                                                sleep(0.05)
+                                                robot.rotateSync(0)
+                                        else:
+                                                print("Rotate 1")
+                                                #robot.driveSync(0)
+                                                robot.rotateSync(1, rotateSpeed)
+                                                sleep(0.05)
+                                                robot.rotateSync(0)
+                                #else: 
+                                        ##robot.rotateSync(0)
+                                        #robot.driveSync(1)
+                                        #sleep(0.1)
+                                        #robot.driveSync(0)
+                                if dir > -999:
+                                        relCount = (1 - abs(dir)) * count
+                                        driveSpeed = int(relCount / 3000 * 50)
+                                        if driveSpeed > 45 :                                        
+                                                robot.driveSync(1, driveSpeed)
+                                        else:
+                                                robot.driveSync(0)
+                                else:
+                                        robot.driveSync(0)
+                                        
+                        # Handle stream
+                        stream.seek(0)
+                        stream.truncate()
+
+                        # Compute fps
+                        lapseTime = (time.time() - startTime)
+                        startTime = time.time()
+                        if lapseTime > 0:
+                                fps = 1.0 / lapseTime
+                                print("fps: " + str(fps))
+
+robot.stop()
+pygame.quit()
