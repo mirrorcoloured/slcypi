c4e2a846b43b4e4d5e2494df37627e4b3cdf8099
pjsch <vharsmistoph@hotmail.com>
2016-12-25 23:59:13 -0700

    updated DataStructures to be 2.7 compatible

--- old3.5/slcyDataStructures.py
+++ old3.5/slcyDataStructures.py
@@ -1,4 +1,4 @@
-# Python 2.7.1
+# Python 3.5.1
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -45,27 +45,23 @@ def plot3d(x, y, z, lines=False, xlabel='X', ylabel='Y', zlabel='Z', title='', c
 class Dataset():
     """List of dicts containing any desired keys
     ex = Dataset([{'time':0,'loc':45}, {'time':1,'loc':48}])"""
-    def __init__(self, datalist=[]): # init with list of dicts
+    def __init__(self, datalist=[]) -> None: # init with list of dicts
         self.list = datalist
-    def len(self):
-        """Returns the number of data entries
-        ex. L = Dataset.len()"""
+    def len(self) -> int:
+        """Returns the number of data entries"""
         return len(self.list)
-    def __len__(self): # overload len() function
+    def __len__(self) -> int: # overload len() function
         return self.len()
-    def dim(self):
-        """Returns the number of keys in the first data entry
-        ex. D = Dataset.dim()"""
+    def dim(self) -> int:
+        """Returns the number of keys in the first data entry"""
         if self.len() > 0:
             return len(self.list[0])
-    def exportdataraw(self, file='dataraw.txt'):
-        """Exports the dataset as a raw text string
-        ex. Dataset.exportdataraw('rawtext.txt')"""
+    def exportdataraw(self, file='temp.data') -> None:
+        """Exports the dataset as a raw text string"""
         with open(file,'w') as f:
             f.write(str(self.list))
-    def exportdatasheet(self, file='datasheet.txt'):
-        """Exports the dataset as a tab-separated format, suitable for pasting into a spreadsheet
-        ex. Dataset.exportdatasheet('datasheet.txt')"""
+    def exportdatasheet(self, file='temp2.data') -> None:
+        """Exports the dataset as a tab-separated format, suitable for pasting into a spreadsheet"""
         o = ''
         headers = list(self.list[0].keys())
         o += '\t'.join(headers)
@@ -76,23 +72,22 @@ class Dataset():
             o = o[:-1]
         with open(file,'w') as f:
             f.write(o)
-    def importdataraw(self, file='dataraw.txt'):
-        """Imports a dataset from a raw text string file
-        ex. Dataset.importdataraw('dataraw.txt')"""
+    def importdata(self, file='temp.data') -> None:
+        """Imports a dataset from a raw text string file"""
         with open(file,encoding='utf8') as f:
             text = f.read()
         self.list = []
         items = text.split('}, {')
-        #print('split input into',len(items),'items:',items)
+        print('split input into',len(items),'items:',items)
         for i in items:
-            #print('processing item:',i)
+            print('processing item:',i)
             for c in "[]{}'":
                 i = i.replace(c,'')
-            #print('finished processing item:',i)
+            print('finished processing item:',i)
             elements = i.split(', ')
             t = {}
             for e in elements:
-                #print('processing element:',e)
+                print('processing element:',e)
                 k,v = e.split(': ')
                 try:
                     v = float(v)
@@ -100,47 +95,41 @@ class Dataset():
                     pass
                 t[k] = v
             self.list.append(t)
-    def plot2d(self, keyx, keyy, title=''):
-        """Generates a 2d plot of data by key
-        ex. Dataset.plot2d('time', 'position', 'Position vs Time')"""
+    def plot2d(self, keyx, keyy, title='') -> None:
         x = [i[keyx] for i in self.list]
         y = [i[keyy] for i in self.list]
         if title == '':
             title = keyy + ' vs ' + keyx
         plot2d(x, y, xlabel = keyx, ylabel = keyy, title = title)
-    def plot3d(self, keyx, keyy, keyz, title=''):
-        """Generates a 3d plot of data by key
-        ex. Dataset.plot3d('time', 'height', 'distance', 'Location by Time')"""
+    def plot3d(self, keyx, keyy, keyz, title='') -> None:
         x = [i[keyx] for i in self.list]
         y = [i[keyy] for i in self.list]
         z = [i[keyz] for i in self.list]
         if title == '':
             title = keyz + ' vs ' + keyy + ' vs ' + keyx
         plot3d(x, y, z, xlabel = keyx, ylabel = keyy, zlabel = keyz, title = title)
-    def max(self,key):
-        """Returns the entry with the maximum value of the selected key
-        ex. M = Dataset.max('temp')"""
+    def max(self,key) -> float:
+        """Returns the entry with the maximum value of the selected key"""
         o = self.list[0]
         for i in self.list:
             if i[key] > o[key]:
                 o = i
         return o
-    def min(self,key):
-        """Returns the entry with the minimum value of the selected key
-        ex. N = Dataset.min('temp')"""
+    def min(self,key) -> float:
+        """Returns the entry with the minimum value of the selected key"""
         o = self.list[0]
         for i in self.list:
             if i[key] < o[key]:
                 o = i
         return o
-    def mean(self,key):
+    def mean(self,key) -> float:
         """Returns the mean value of the selected key
-        ex. A = Dataset.mean('temp')"""
+        key <str>"""
         s = self.sublist(key)
         return sum(s) / len(s)
-    def linearmap(self, oldkey, newkey, r1, r2):
+    def linearmap(self, oldkey:str, newkey:str, r1:list, r2:list) -> None:
         """Maps values of key in range r1 to range r2 using a linear transformation
-        ex. Dataset.linearmap('radians', 'degrees', [0,3.1415], [0,180])"""
+        ex.linearmap('radians', 'degrees', [0,3.1415], [0,180])"""
         r1n, r1x = r1
         r2n, r2x = r2
         d1 = r1x - r1n
@@ -149,9 +138,8 @@ class Dataset():
         fprestring = '(('
         fpoststring = ' - ' + str(r1n) + ') * (' + str(d2) + ' / ' + str(d1) +')) + '+ str(r2n)
         self.addcalc(newkey, oldkey, fprestring, fpoststring)
-    def calcsd(self, key):
-        """Adds SD calculation based on specified key
-        ex. Dataset.calcsd('measurement')"""
+    def calcsd(self, key:str) -> None:
+        """Adds SD calculation based on specified key"""
         mean = self.mean(key)
         va = []
         for i in self.list:
@@ -163,37 +151,35 @@ class Dataset():
         sd = var ** (1/2)
         self.__sd__ = sd
         self.addcalc(key + '_sd', key, '(', '-' + str(mean) + ')/' + str(sd))
-    def calcdelta(self, key):
+    def calcdelta(self, key:str) -> None:
         """Adds delta calculation of specified key
-        delta = abs(value - mean)
-        ex. Dataset.calcdelta('distance')"""
+        delta = abs(value - mean)"""
         mean = self.mean(key)
         self.addcalc(key + '_delta', key, 'abs(', '-' + str(mean) + ')')
-    def renamekey(self, oldkey, newkey):
+    def renamekey(self, oldkey:str, newkey:str) -> None:
         """Renames a key
-        ex. Dataset.renamekey('time', 't')"""
+        ex.renamekey('time', 't')"""
         for i in self.list:
             i[newkey] = i[oldkey]
             i.pop(oldkey)
-    def copy(self):
-        """Returns a new copy of this Dataset
-        ex. D = Dataset.copy()"""
+    def copy(self) -> 'Dataset':
+        """Returns a new copy of this Dataset"""
         c = Dataset()
         for i in self.list:
             c.append(i)
         return c
-    def add(self, **data):
+    def add(self, **data) -> None:
         """Adds a data element to the set
-        ex. Dataset.add(time = 2, loc = 52)"""
+        ex.add(time = 2, loc = 52)"""
         self.list.append(data)
-    def addcalc(self, newkey, oldkey, precalc='', postcalc=''):
+    def addcalc(self, newkey, oldkey, precalc='', postcalc='') -> None:
         """Adds a key to existing items based on existing keys
-        ex. Dataset.addcalc('tplusonesquared', 't', '(', '+1)**2')"""
+        ex.addcalc('tplusonesquared', 't', '+1)**2', '(')"""
         for i in self.list:
             i[newkey] = eval(precalc+str(i[oldkey])+postcalc)
-    def append(self, data):
+    def append(self, data) -> None:
         """Appends data to the set
-        ex. Dataset.append({'time':2, 'loc':52})
+        ex.append({'time':2, 'loc':52})
         Accepts other Dataset objects, dict objects, or lists of dict objects"""
         if type(data) is Dataset:
             self.append(data.list)
@@ -204,9 +190,9 @@ class Dataset():
             self.list.append(data)
         else:
             print('Invalid data type supplied. Accepts Dataset, dict, or list of dicts.')
-    def removeitems(self, **conditions):
+    def removeitems(self, **conditions) -> None:
         """Removes data elements from the set meeting criteria
-        ex. Dataset.remove(time='>1', 'loc'='==50')
+        ex.remove(time='>1', 'loc'='==50')
         Multiple conditions are AND gated, call function again for OR usage"""
         for i in self.list:
             for c in conditions:
@@ -215,15 +201,15 @@ class Dataset():
                     rem = True
             if rem:
                 self.list.remove(i)
-    def removekeys(self, *keys):
+    def removekeys(self, *keys) -> None:
         """Removes specified keys from data elements
-        ex. Dataset.remove('time')"""
+        ex.remove('time')"""
         for k in keys:
             for i in self.list:
                 i.pop(k)
-    def subset(self, *keys):
+    def subset(self, *keys) -> 'Dataset':
         """Returns a subset of this dataset containing the requested keys
-        ex. L = Dataset.subset('loc')"""
+        locs = ex.subset('loc')"""
         o = []
         for i in self.list:
             d = {}
@@ -231,31 +217,29 @@ class Dataset():
                 d[k] = i[k]
             o.append(d)
         return Dataset(o)
-    def sublist(self, key):
+    def sublist(self, key) -> list:
         """Returns a list of the requested key values
-        ex. L = Dataset.sublist('loc')"""
+        locs = ex.subset('loc')"""
         o = []
         for i in self.list:
             o.append(i[key])
         return o
-    def getkeys(self):
-        """Returns a list of keys in the set
-        ex. K = Dataset.getkeys()"""
+    def getkeys(self) -> list:
+        """Returns a list of keys in the set"""
         return list(self.list[0].keys())
-    def prnt(self, *keys):
+    def print(self, *keys) -> None:
         """Prints the contents of the dataset, optionally only certain keys
-        ex. Dataset.prnt()
-        ex. Dataset.prnt('time', 'position')"""
+        ex.print('time')"""
         if len(keys) > 0:
             for i in self.list:
                 firstkey = True
-                sys.stdout.write('{',end='')
+                print('{',end='')
                 for k in keys:
                     if firstkey:
-                        sys.stdout.write("'"+str(k)+"'"+': '+str(i[k]),end='') # appears like a dict object
+                        print("'"+str(k)+"'"+': '+str(i[k]),end='') # appears like a dict object
                         firstkey = False
                     else:
-                        sys.stdout.write(',',"'"+str(k)+"'"+': '+str(i[k]),end='')
+                        print(',',"'"+str(k)+"'"+': '+str(i[k]),end='')
                 print('}')
         else:
             for i in self.list:
@@ -266,13 +250,13 @@ def DatasetDemo():
     d.add(time=1,x=2,y=3)
     d.add(time=2,x=1,y=4)
     d.add(time=3,x=5,y=0)
-    d.prnt()
+    d.print()
     d.removeitems(time='>1',x='<3')
     print('remove time>1 AND x<3')
     print('add time squared')
     d.addcalc('timesquared','time','','**2')
     d.addcalc('justone','time','1+0*','')
     d.calcsd('x')
-    d.prnt()
+    d.print()
     return d
 d = DatasetDemo()
